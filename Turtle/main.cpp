f7e147d9e3079cd1489a1fb6f7df5d5c99b1d760
jasper van der vaart <jasper.vaart@live.nl>
2021-05-10 15:40:03 +0200

    added edges output (buggy)

--- Turtle/main.cpp
+++ Turtle/main.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <easy3d/core/vec.h>
 #include <easy3d/core/mat.h>
+#include <boost/graph/adjacency_list.hpp>
 
 #include "3rd_party/nlohmann/json.hpp"
 
@@ -36,6 +37,7 @@ public:
         fValue = other.fValue;
         rollValue = other.rollValue;
         rollValue = other.rotateValue;
+
     }
 
     /// output the current location of the turtle ///
@@ -61,8 +63,12 @@ public:
         return storedPoints;
     }
 
+    std::vector<std::vector<unsigned int>> getStoredEdges(){
+        return storedEdges;
+    }
+
     /// store internalized points to file ///
-    void writeToFile(const std::string& fileName){
+    void writeToXYZ(const std::string& fileName){
         std::ofstream storageFile;
         storageFile.open(fileName);
         storageFile << "x y z\n";
@@ -72,10 +78,36 @@ public:
         storageFile.close();
     }
 
+    void writeToPly(const std::string& fileName){
+        std::ofstream storageFile;
+        storageFile.open(fileName);
+        storageFile << "ply" << std::endl;
+        storageFile << "format ascii 1.0" << std::endl;
+        storageFile << "element vertex " << storedPoints.size() << std::endl;
+        storageFile << "property float x" << std::endl;
+        storageFile << "property float y" << std::endl;
+        storageFile << "property float z" << std::endl;
+        storageFile << "element edge " << storedEdges.size() << std::endl;
+        storageFile << "property int vertex1" << std::endl;
+        storageFile << "property int vertex2" << std::endl;
+        storageFile << "end_header" << std::endl << std::endl;
+
+        for (easy3d::Vec<3, double> p: storedPoints){
+            storageFile << p << std::endl;
+        }
+
+        storageFile << std::endl;
+        for (std::vector<unsigned int> i: storedEdges){
+            storageFile << i[0] << " " << i[1] << std::endl;
+        }
+        storageFile.close();
+    }
+
     /// read json file ///
     void readFile(nlohmann::json j){
         setDefaultValues(j["dimensions"]);
         std::string line = translateLine(j["axiom"], j["rules"], j["recursions"]);
+
         readLine(line);
     }
 
@@ -89,13 +121,13 @@ private:
     // collection of stored points
     std::vector<easy3d::Vec<3, double>> storedPoints;
 
+    // collection of stored edges
+    std::vector<std::vector<unsigned int>> storedEdges;
+
     // default variables
     double fValue = 5;
     double rotateValue = 10;
     double rollValue = 10;
-
-    // collection of edges
-    //TODO do this
 
     //debug prints location at every step
     bool debug = false;
@@ -142,6 +174,10 @@ private:
     /// internalize current location ///
     void storeLoc(){
         storedPoints.emplace_back(loc);
+    }
+
+    void storeEdge(){
+
     }
 
     void setDefaultValues(nlohmann::json d){
@@ -202,6 +238,12 @@ private:
 
     /// translate the "simple" line to 3d points ///
     void readLine(std::string line){
+        // store starting point
+        storeLoc();
+
+        unsigned int basepoint = 0;
+        bool returnEdge = false;
+
         for(int i = 0; i < line.size(); ++i) {
             double override = 0;
             int j = 0;
@@ -225,13 +267,33 @@ private:
                 for (int k = i; k < line.size(); ++k) {
                     if (line[k] == ']' && oNested == cNested){
                         Turtle turtle(*this);
+
                         if (debug){turtle.setDebug();}
+
                         turtle.readLine(line.substr(i + 1, k - i - 1));
 
-                        for (easy3d::Vec<3, double> p: turtle.getStoredPoints()) {
-                            storedPoints.emplace_back(p);
+                        unsigned int connection = storedPoints.size() - 1;
+                        for (int l = 1; l < turtle.getStoredPoints().size() ; ++l) {
+                            storedPoints.emplace_back(turtle.getStoredPoints()[l]);
                         }
 
+                        std::vector<std::vector<unsigned int>> tEdgeList;
+
+                        for (int l = 1; l < turtle.getStoredEdges().size(); ++l) {
+                            std::vector<unsigned int> edge = turtle.getStoredEdges()[l];
+                            std::vector<unsigned int> nEdge = {edge[0] + connection, edge[1] + connection};
+                            tEdgeList.emplace_back(nEdge);
+                        }
+
+                        std::vector<unsigned int> connector = {connection, connection + 1};
+                        storedEdges.emplace_back(connector);
+
+                        for (const auto& l: tEdgeList){
+                            storedEdges.emplace_back(l);
+                        }
+
+
+                        returnEdge = true;
                         break;
                     } else if (line[k] == ']'){
                         oNested ++;
@@ -246,15 +308,30 @@ private:
             }
 
             if (line[i] == 'F') {
-                if (override == 0){
-                    stepForward(fValue);
-                }
-                else {
-                    stepForward(override);
-                }
+                // take a step
+                if (override == 0){stepForward(fValue);}
+                else {stepForward(override);}
                 storeLoc();
 
                 if (debug){printLocation();}
+
+
+                if (returnEdge){
+                    // if the point lies after a nesting a correct link has to be set
+                    unsigned int connection = getStoredPoints().size();
+                    std::vector<unsigned int> edge = {basepoint - 1 , connection - 1};
+                    storedEdges.emplace_back(edge);
+
+                    returnEdge = false;
+                } else if (getStoredPoints().size() == 1){
+                    // if the first point in a branch no edges are created
+                    continue;
+                } else {
+                    unsigned int connection = getStoredPoints().size();
+                    std::vector<unsigned int> edge = {connection - 2, connection - 1};
+                    storedEdges.emplace_back(edge);
+                }
+                basepoint = getStoredPoints().size();
 
             } else if (line[i] == '+') {
                 if (override == 0){
@@ -292,15 +369,14 @@ private:
             i += j;
         }
     }
-
-    //TODO edge store
-    //TODO edge write
 };
 
 
 int main() {
     std::string inputPath = "../test_inputs/in_1.json";
     std::string outputPath = "../export.xyz";
+    std::string outputPath2 = "../export.ply";
+
 
     std::ifstream treeFile(inputPath, std::ifstream::binary);
     nlohmann::json j;
@@ -311,5 +387,6 @@ int main() {
     //turtle.setDebug();
     turtle.readFile(j);
 
-    turtle.writeToFile(outputPath);
+    turtle.writeToXYZ(outputPath);
+    turtle.writeToPly(outputPath2);
 }
