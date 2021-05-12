ef42ea0c18b507ce5364b162b30513398f6fe93d
Lars Langhorst <l.m.langhorst@student.tudelft.nl>
2021-05-12 19:15:18 +0200

    almost workinggit add main.cpp git add main.cpp !

--- skeletor/main.cpp
+++ skeletor/main.cpp
@@ -87,7 +87,7 @@ int read_object(std::string  input, std::map<int, Point > &Vertices,
         auto p2 = Point(x2,y2,z2);
         Vertices[ID1] = p1;
         Vertices[ID2] = p2;
-        std::vector vectorBranch(ID1, ID2);
+        std::vector vectorBranch{ID1, ID2};
         Branches[IDB] = vectorBranch;
         BranchRadii[IDB] = r;
         EndBranch[IDB] = e;
@@ -99,7 +99,6 @@ int read_object(std::string  input, std::map<int, Point > &Vertices,
 
 
     }
-    std::cout<<" reader done " <<std::endl;
 
     return 0;
 }
@@ -108,7 +107,7 @@ int read_object(std::string  input, std::map<int, Point > &Vertices,
 
 
 
-void writeJSON( std::map<int, Point> &Vertices, std::map<int, std::vector<int>> &Branches,
+int writeJSON( std::map<int, Point> &Vertices, std::map<int, std::vector<int>> &Branches,
                 std::map<int, float> &BranchRadii, std::map<int, int> &EndBranch, std::string output_file, float maxradius){
     json final;
     final["type"] = "CityJSON";
@@ -116,45 +115,65 @@ void writeJSON( std::map<int, Point> &Vertices, std::map<int, std::vector<int>> 
 
 
     //creating object with all vertices
+    std::cout<<"creating object with all vertices "<<std::endl;
     json vertices= json::array();
     
-    for (int i=1; i < Vertices.size(); i++){
+    for (int i=0; i < Vertices.size(); i++){
         std::vector<float> array1{Vertices[i].x, Vertices[i].y, Vertices[i].z};
         json subvertices= json::array();
         for (int j = 0; j < array1.size(); j++){
                subvertices.emplace_back(array1[j]);
-    }
+        }
     vertices.emplace_back(subvertices);
-}
+    }
 
     //creating boundaries
-    json boundaries;
-    for (int i=1; i < Branches.size(); i++){
-        boundaries.emplace_back(Branches[i][0], Branches[i][1]);
+    std::cout<<"creating boundaries"<<std::endl;
+
+    json boundaries= json::array();
+    for (int i=0; i < Branches.size(); i++){
+        json branch = json::array();
+        branch.emplace_back(Branches[i][0]);
+        branch.emplace_back(Branches[i][1]);
+        boundaries.emplace_back(branch);
     }
 
+
     //create semantic classes
+    std::cout<<"create semantic classes"<<std::endl;
     json semantics;
     json types;
     for (int i=1; i < 11; i++){
-        float radius = maxradius/i;
+        float radius = (maxradius*i)/11;
         json classtemp { { "class",i, "radius",radius} };
         types.emplace_back(classtemp);
     }
-    json typestemp{ {"class", 11}, {"radius", maxradius / 11 } };
+    json typestemp{ {"class", 11}, {"radius", maxradius / 12 } };
     types.emplace_back(typestemp); // cleass for ends of branches
 
     //assigning branches to classes and putting them into values array
+    std::cout<<"assigning branches to classes and putting them into values array"<<std::endl;
     json values;
-    for (int i=1; i < BranchRadii.size(); i++){
-        for (int j=1; j < 11; j++){
-            if (BranchRadii[i] < maxradius/j && BranchRadii[i] >= maxradius/(j+1)){
+    for (int i=0; i < BranchRadii.size(); i++){
+        for (int j=1; j <= 11; j++){
+            if ((BranchRadii[i] > ((maxradius*j-1)/11)) && (BranchRadii[i] <= (maxradius*(j)/11))){
                 values[i] = j;
             }
+                // if( ((BranchRadii[i] < ((maxradius)/11)))){
+                //      values[i] = 1;
+                // }
+            }
         }
-    }
+            
+
+
+        
+        
+    
 
     //assign ends of branches
+    std::cout<<"assign ends of branches"<<std::endl;
+
     for (int i=1; i < EndBranch.size(); i++){
         if (EndBranch[i] == 1){
             values[i] = 11;
@@ -162,23 +181,29 @@ void writeJSON( std::map<int, Point> &Vertices, std::map<int, std::vector<int>> 
     }
 
     //write semantics
+    std::cout<<"write semantics"<<std::endl;
+
     semantics["types"] = types;
     semantics["values"] = values;
 
     //creating full geometry
+    std::cout<<"creating full geometry"<<std::endl;
     json geometry {{"type", "MultilineString"}, {"lod", 2},
                    {"boundaries", boundaries}, {"semantics", semantics} };
+    final["geometry"] = geometry;
 
 
 
-
-
+    std::cout<<"Writing vertices.."<<std::endl;
     final["vertices"] = vertices;
+    std::cout<<"vertices written"<<std::endl;
 
     std:: ofstream MyFile(output_file);
+    std::cout<<"dumping file..."<<std::endl;
     MyFile << final.dump(4);
     MyFile.close();
-
+    std::cout<<"file closed"<<std::endl;
+    return 0;
 }
 
 int main()
@@ -187,7 +212,7 @@ int main()
     std::map<int, std::vector<int>> Branches;
     std::map<int, float> BranchRadii;
     std::map<int, int> EndBranch;
-    float maxradius;
+    float maxradius = 0.01;
     const char *file_in = "data.txt";
     std:: string  input =  file_in;
     input = "../" + input;
@@ -196,6 +221,9 @@ int main()
     std:: string  output =  file_out;
     output = "../" + output;
     read_object(input, Vertices, Branches, BranchRadii, EndBranch, maxradius);
+    std::cout<<" reader done " <<std::endl;
     writeJSON(Vertices, Branches, BranchRadii, EndBranch, output, maxradius);
+    std::cout<<"return"<<std::endl;
+    return 0;
 
 }
